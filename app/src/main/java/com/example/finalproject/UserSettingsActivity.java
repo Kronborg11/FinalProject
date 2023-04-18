60866099d712326c6d98221ce9ec0b5ea8766673
jense <jensenconnor@ymail.com>
2023-04-17 22:48:32 -0400

    sdfg

--- app/src/main/java/com/example/finalproject/UserSettingsActivity.java
+++ app/src/main/java/com/example/finalproject/UserSettingsActivity.java
@@ -7,15 +7,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class UserSettingsActivity extends AppCompatActivity {
+public class UserSettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
 
+        Spinner spinnerSex = findViewById(R.id.spinnerSex);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sex, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSex.setAdapter(adapter);
+        spinnerSex.setOnItemSelectedListener(this);
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
